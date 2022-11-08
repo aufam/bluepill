c80f56fb9ae28a3da85fe652d41e53976fdd3476
m.aufa <aufamuhammad715@gmail.com>
2022-11-08 20:21:33 +0700

    Changing to cmake

--- Project/os.cc
+++
@@ -1,7 +0,0 @@
-#include "os.h"
-
-/// overload operator new with malloc from heap_4
-void *operator new(size_t size) { return pvPortMalloc(size); }
-
-/// overload operator delete with free from heap_4
-void operator delete(void *ptr) { vPortFree(ptr); }
